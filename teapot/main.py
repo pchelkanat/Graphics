1a738d3ee38155dff2e86446f65acaa6b8db29e8
pchelkanat <pchelkanat@mail.ru>
2017-12-04 03:18:26 +0300

    add file

---
+++ teapot/main.py
@@ -0,0 +1,8 @@
+from teapot.animation_teapot import animation_main
+from teapot.dotted_teapot import dotted_main
+from teapot.triangular_teapot import triangular_main
+
+if __name__ == '__main__':
+    triangular_main()
+    dotted_main()
+    #animation_main()
