23c9abd537eaceceb438726df43b39498669cb50
pchelkanat <pchelkanat@mail.ru>
2017-12-06 15:39:34 +0300

    исправлены ошибки, замена на i*alpha

--- teapot/animation_teapot.py
+++ teapot/animation_teapot.py
@@ -5,44 +5,65 @@ from teapot.triangular_teapot import triangle
 
 
 # ----------Для анимации
-
 def drawFrames(image, vertices, faces, delta, alpha, num_scale, frames_count, green, red, height, width):
     ims = []
+    """
     for i in range(1, frames_count + 1, ):
         # преобразуем в проективные СК, добавляем координату w
         newvertices = transform(vertices)
         # трансформируем
-        translates = np.dot(newvertices, translation(-delta[0], -delta[1]))
-        if (i * num_scale / frames_count) <= (2 * np.pi):
+        translates = np.dot(newvertices, translation(-delta[0],-delta[1]))
+        if (I*ALPHA / frames_count) <= (2 * np.pi): #Ошибка была в i*num_scale, но нужно же по градусам
             scales = np.dot(translates, scale(i * num_scale / frames_count))
         else:
             scales = np.dot(translates, scale(i * (1 / num_scale) / frames_count))
         rotates = np.dot(scales, rotation(i * alpha / frames_count))
-        detransletes = np.dot(rotates, translation(delta[0], delta[1]))
+        detransletes = np.dot(rotates, translation(delta[0],delta[1]))
         # переводим в декартовую
         vertices = retransform(detransletes)
         # vertices=matrix(vertices,delta,num_scale, alpha)
+    """
+    for i in range(1, frames_count + 1):
+        # преобразуем в проективные СК, добавляем координату w
+        newvertices = transform(vertices)
+        # трансформируем:
+        # Так как мы переопределили позиции Ex, Ey, то произведем обратный порядок умножения
+        # ретрансляция
+        translates = np.dot(newvertices, np.linalg.inv(translation(delta)))
+        # поворот
+        rotates = np.dot(translates, scale(i * alpha / frames_count))
+        # масштаб
+        # если угол <=2pi, то мы уменьшаем, иначе увеличиваем
+        if (i * alpha / frames_count) <= (2 * np.pi):
+            scales = np.dot(rotates, np.linalg.inv(scale(i * num_scale / frames_count)))
+        else:
+            scales = np.dot(rotates, scale(i * num_scale / frames_count))
+        # трансляция
+        detransletes = np.dot(scales, translation(delta))
+        # переводим в декартовую
+        vertices = retransform(detransletes)
 
+        # ???Раскраска
         color = coloring(green, red)
         image = triangle(image, faces, vertices, color)
 
+        #Заносим в массив фреймов
         im = plt.imshow(image, animated=True)
         ims.append([im])
         print("Frame", i, " creation finished.")
 
         # обнуляем картинку
         image = np.zeros((height, width, 3), dtype=np.uint8)
-
     return ims
 
 
 def animation_main():
     vertices, faces = read("teapot.obj")
-    height, width = 800, 800
+    height, width = 400, 400
 
-    frames_count = 7  # с увеличением фреймов уменьшается чайник. min=6, после выходит за рамки
+    frames_count = 10  # с увеличением фреймов уменьшается чайник. min=6, после выходит за рамки
     num_scale = 2
-    delta = [height / 2, width / 2]
+    delta = [height / 2, width / 2]  #не менять!
     alpha = 2 * np.pi
 
     image = np.zeros((height, width, 3), dtype=np.uint8)
